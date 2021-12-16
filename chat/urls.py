608182dcb4d01e05c708ea069d32921812ab66b8
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-12-16 14:53:22 +0330

    v20-m20:00

--- chat/urls.py
+++ chat/urls.py
@@ -5,5 +5,8 @@ from django.contrib.auth import views as auth_views
 app_name = 'chat'
 
 urlpatterns = [
-	path('', test, name='test'),
+	path('', index, name='index'),
+	path('create/', create_chat, name='create_chat'),
+	path('<str:chat_id>/', chat, name='chat'),
+	path('<str:chat_id>/leave/', leave_chat, name='leave_chat'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,10 +1,17 @@
 import json
+from django.urls import reverse
 from django.utils.safestring import mark_safe
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import UserRegisterForm
+from .models import GroupCaht, Member
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+
+
+from channels.layers import get_channel_layer
+from asgiref.sync import async_to_sync
+
 
 # Create your views here.
 def register(request):
@@ -34,3 +41,76 @@ def register(request):
 def index(request):
 	current_user = request.user
 	return render(request, 'chat/index.html', {'member': current_user.member_set.all()})
+
+
+@login_required
+def create_chat(request):
+	current_user = request.user
+	title = request.POST['group_name']
+	chat = GroupCaht.object.create(creator_id=current_user.id, title=title)
+	Member.object.create(chat_id=chat.id, user_id=current_user.id)
+	return redirect(reverse('chat:chat', args=[chat.unique_code]))
+
+@login_required
+def chat(request, chat_id):
+	current_user = request.user
+	try:
+		chat = GroupCaht.objects.get(unique_code=chat_id)
+	except GroupCaht.DoesNotExist:
+		return render(request, 'chat/404.html')
+	if request.method == 'GET':
+		if Member.objects.filter(chat_id=chat.id, user_id=current_user.id).count() == 0:
+			return render(request, 'chat/join_chat.html', {'chatObject': chat })
+
+		return render(request, 'chat/chat.html', {'chatObject': chat, 'chat_id_json': mark_safe(json.dumps(chat.unique_code))})
+
+	elif request.method == "POST":
+		Member.objects.create(chat_id=chat.id, user_id=current_user.id)
+
+		channel_layer = get_channel_layer()
+		async_to_sync(channel_layer.group_send)(
+			f"chat_{chat.unique_code}",
+			{
+				'type': 'chat_activity',
+				'message': json.dumps({'type': 'join', 'username': current_user.username})
+			}
+		)
+
+		return render(request, 'chat/chat.html', {'chatObject': chat, 'chat_id_json': mark_safe(json.dumps(chat.unique_code))})
+
+@login_required
+def leave_chat(request, chat_id):
+	current_user = request.user
+	try:
+		chat = GroupCaht.objects.get(unique_code=chat_id)
+	except GroupCaht.DoesNotExist:
+		return render(request, 'chat/404.html')
+
+	if chat.creator_id == current_user.id:
+		chat.delete()
+
+		channel_layer = get_channel_layer()
+
+		async_to_sync(channel_layer.group_send)(
+			f"chat_{chat.unique_code}",
+			{
+				'type': 'chat_activity',
+				'message': json.dumps({'type': 'delete'})
+			}
+		)
+
+	else:
+		Member.objects.filter(chat_id=chat.id, user_id=current_user.id).delete()
+
+
+			
+		channel_layer = get_channel_layer()
+        async_to_sync(channel_layer.group_send)(
+            f"chat_{chat.unique_code}",
+            {
+                'type': 'chat_activity',
+                'message': json.dumps({'type': "leave", 'username': current_user.username})
+            }
+        )
+
+    return redirect('chat:index')	
